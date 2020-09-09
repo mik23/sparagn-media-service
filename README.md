af780378049c4a88a06799c3a7bdba47bf61b926
Michael <marcotrigiano.m@gmail.com>
2020-09-09 21:00:02 +0100

    Init Go Sparagn Media Service

---
+++ README.md
@@ -0,0 +1,9 @@
+### Requirements
+Install go1.15.1 darwin/amd64 ( see https://golang.org/doc/install)
+
+#### How to test
+go test ./...
+
+go clean
+go build main.go
+./main (or "go run main.go")
---
+++ integer/sum_test.go
@@ -0,0 +1,21 @@
+package integer
+
+import "testing"
+
+func TestSum(t *testing.T) {
+
+	t.Run("Collection of 5 integers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+
+		got := Sum(numbers)
+		want := 15
+
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
+	})
+
+
+
+
+}
