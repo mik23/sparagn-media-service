2c7f9860ccf2b8854a7d539ab6beeb74f117271f
Michael <marcotrigiano.m@gmail.com>
2020-12-16 19:42:41 +0000

    update dev deploy ip and pipeline test

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,9 +3,9 @@
 #This script is meant to be used to deploy the code remotely and from the circleCi pipelines
 #Prerequirement: enable ssh and put public key into the server
 
-#IP=35.230.96.96
-#SSH_USERNAME=sparagn
-#PROJECT_NAME=sparagn-rest-service
+# IP=93.38.115.75
+# SSH_USERNAME=sparagn
+# PROJECT_NAME=sparagn-media-service
 
 if [ -z "${CIRCLE_BRANCH}" ]
 then
@@ -18,9 +18,9 @@ source docker/.env.${CIRCLE_BRANCH}
 zip -r $PROJECT_NAME .
 
 #send the zip to the server
-scp -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $PROJECT_NAME.zip $SSH_USERNAME@$IP:/home/sparagn
+scp -P 2222 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $PROJECT_NAME.zip $SSH_USERNAME@$IP:/home/sparagn
 
-echo sparagn | ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $SSH_USERNAME@$IP bash -c "'
+echo sparagn | ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p 2222 $SSH_USERNAME@$IP bash -c "'
   export CIRCLE_BRANCH=${CIRCLE_BRANCH}
   if [ -d $PROJECT_NAME ]
   then
